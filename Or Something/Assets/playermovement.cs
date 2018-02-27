cf571b500ee37641cfa40fa1df0ea86538399bd7
Fernando de Paz <fdepaz@uci.edu>
2018-02-27 13:04:12 -0800

    characters and Animations

---
+++ Or Something/Assets/playermovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private bool ready=true;
+    public Animator anim;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+
+
+        if (Input.GetMouseButton(0) && ready)
+        {
+            StartCoroutine(animweight());
+        }
+    }
+
+
+    IEnumerator animweight()
+    {
+        anim.SetTrigger("punch");
+        ready = false;
+        yield return new WaitForSeconds(1f);
+        ready = true;
+    }
+}
---
+++ Or Something/Assets/playerrotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerrotate : MonoBehaviour {
+    public float rotationspeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        if (h > 0) {
+            transform.rotation = Quaternion.Euler(0, 0, 102.547f);
+        }
+	}
+}
