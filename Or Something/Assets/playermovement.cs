663ef5f5f1efc4bc7a29d40c24315bcc3f253361
Fernando de Paz <fdepaz321@gmail.com>
2018-02-27 14:20:58 -0800

    Added Effects and Animation

--- Or Something/Assets/playermovement.cs
+++ Or Something/Assets/playermovement.cs
@@ -2,42 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermovement : MonoBehaviour {
+public class playermovement : MonoBehaviour
+{
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
-    private bool ready=true;
+    private bool ready = true;
     public Animator anim;
+    CharacterController controller;
+    public float h = 0;
+    bool canJump = true;
+    bool locked = false;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
+        h = Input.GetAxis("Horizontal");
+
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,0);
+            moveDirection = new Vector3(h, 0, 0);
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
+            if (Input.GetButton("Jump") && canJump)
+            {
+                StartCoroutine(jumpWait());
+            }
+
 
         }
+        else
+        {
+
+            float oldY = moveDirection.y;
+            moveDirection = new Vector3(h, 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            moveDirection.y = oldY;
+
+        }
+        if (locked)
+        {
+            moveDirection = Vector3.zero;
+        }
+
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
 
-
+        anim.SetFloat("Speed", Mathf.Abs(moveDirection.x));
 
         if (Input.GetMouseButton(0) && ready)
         {
             StartCoroutine(animweight());
         }
     }
-
+    IEnumerator jumpWait()
+    {
+        locked = true;
+        canJump = false;
+        anim.SetTrigger("jump");
+        yield return new WaitForSeconds(.5f);
+        locked = false;
+        moveDirection = new Vector3(h, 0, 0);
+        moveDirection.y = jumpSpeed;
+        controller.Move(moveDirection * Time.deltaTime);
+        yield return new WaitForSeconds(1f);
+        canJump = true;
+    }
 
     IEnumerator animweight()
     {
+        locked = true;
         anim.SetTrigger("punch");
         ready = false;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.8f);
         ready = true;
+        locked = false;
     }
 }
--- Or Something/Assets/playerrotate.cs
+++ Or Something/Assets/playerrotate.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerrotate : MonoBehaviour {
-    public float rotationspeed;
+    public float rotationspeed = .5f;
+    private playermovement pm;
+    Vector3 faceForward;
+    Vector3 faceBackward;
+    private Animator anim;
 	// Use this for initialization
 	void Start () {
-		
+        pm = GetComponentInParent<playermovement>();
+        faceForward = new Vector3(0, 120f, 0 );
+        faceBackward = new Vector3(0, -120f , 0);
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float h = Input.GetAxis("Horizontal");
+        float h = pm.h;
         if (h > 0) {
-            transform.rotation = Quaternion.Euler(0, 0, 102.547f);
+            if (Quaternion.Angle(transform.rotation, Quaternion.Euler(faceForward)) > 1){
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(faceForward), rotationspeed * Time.deltaTime);
+                anim.SetBool("Mirror", false);
+            }
+
         }
-	}
+        if (h < 0)
+        {
+
+            if (Quaternion.Angle(transform.rotation, Quaternion.Euler(faceBackward)) > 1)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(faceBackward), rotationspeed * Time.deltaTime);
+                anim.SetBool("Mirror", true);
+            }
+        }
+    }
 }
